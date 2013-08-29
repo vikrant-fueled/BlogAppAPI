3e7166b50a22d5345f938c28a32450f4d91e1a77
Vikrant Pogula <vtp@fueled.co>
2013-08-29 16:12:48 +0530

    Removed *.pyc from git and minor bugfix in : Admin module

--- BlogApp/admin.py
+++ BlogApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from BlogApp.models import UserProfile, BlogPost, BlogDraft
+from BlogApp.models import UserProfile, BlogPost, PostDraft, Comment, Reply
 
 admin.site.register(UserProfile)
 admin.site.register(BlogPost)
-admin.site.register(BlogDraft)
+admin.site.register(PostDraft)
+admin.site.register(Comment)
+admin.site.register(Reply)
